0bcfa72b085511d2e34937c6db1b3bd35df32702
manuandru <manuandru987@gmail.com>
2022-01-26 19:39:38 +0100

    Adjust script to deploy

---
+++ boot.py
@@ -0,0 +1,9 @@
+# This file is executed on every boot (including wake-boot from deepsleep)
+#import esp
+#esp.osdebug(None)
+#import webrepl
+#webrepl.start()
+
+import src.main as main
+
+main.run()
--- src/main.py
+++ src/main.py
@@ -1,6 +1,21 @@
 from src.utils.Sensor import Sensor
+import src.utils.WIFIsetup as wifi
+import src.utils.MQTTsetup as mqtt
+import time
 
-sensor = Sensor(4)
+def run():
+    # DHT11
+    sensor = Sensor(4)
 
-sensor.measure()
-print(f'Temperature: {sensor.get_temperature()}')
+    # WIFI
+    wifi.connect()
+
+    # MQTT
+    client = mqtt.create()
+    client.connect()
+
+    while True:
+        sensor.measure()
+        client.publish('bedroom/temperature', f'{sensor.get_temperature()}')
+        client.publish('bedroom/humidity', f'{sensor.get_humidity()}')
+        time.sleep(1)
